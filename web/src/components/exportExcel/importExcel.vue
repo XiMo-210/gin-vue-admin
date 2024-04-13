eb987009b4a3d0e6f54488d520816387034524bf
XiMo-210 <2831802697@qq.com>
2024-04-13 17:05:00 +0800

    feat: 添加导入等待加载效果

--- web/src/components/exportExcel/importExcel.vue
+++ web/src/components/exportExcel/importExcel.vue
@@ -1,7 +1,10 @@
 <template>
   <el-upload
+    v-loading.fullscreen.lock="fullscreenLoading"
+    element-loading-text="学生信息导入中，请耐心等待..."
     :action="url"
     :show-file-list="false"
+    :before-upload="beginLoad"
     :on-success="handleSuccess"
     :multiple="false"
   >
@@ -14,6 +17,7 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { ElMessage } from 'element-plus'
 
 const baseUrl = import.meta.env.VITE_BASE_API
@@ -29,6 +33,12 @@ const emit = defineEmits(['on-success'])
 
 const url = `${baseUrl}/sysExportTemplate/importExcel?templateID=${props.templateId}`
 
+const fullscreenLoading = ref(false)
+
+const beginLoad = () => {
+  fullscreenLoading.value = true
+}
+
 const handleSuccess = (res) => {
   if (res.code === 0) {
     ElMessage.success('导入成功')
@@ -36,5 +46,7 @@ const handleSuccess = (res) => {
   } else {
     ElMessage.error(res.msg)
   }
+  fullscreenLoading.value = false
 }
+
 </script>
